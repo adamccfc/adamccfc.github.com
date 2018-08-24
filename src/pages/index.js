5997159658aab627852a90dca20e927a84d9c515
Adam Morgan <adamccfc@gmail.com>
2018-08-24 10:25:50 +0100

    Merge branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,45 +13,43 @@ library.add(fas, far, fab)
 const IndexPage = () => (
   <Layout>
     <div className="home-container">
-      <div className="container">
-        <div className="home-container__intro">
-          <h1><span className="animation animation__fade-in-down">Adam </span><span className="animation animation__fade-in-down animation__delay--0-5">Morgan</span></h1>
-          <h2><span className="animation animation__fade-in-down animation__delay--1">Front end </span><span className="animation animation__fade-in-down animation__delay--1-5">developer</span></h2>
-        </div>
-        <p class="contact animation animation__fade-in-up animation__delay--2">Need a website built? Get in touch.</p>
-        <ul className="icons-list animation animation__fade-in-up animation__delay--2">
-          <li className="icons-list__twitter">
-            <a href="http://www.twitter.com/adamtaffymorgan" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={["fab","twitter"]} />
-            </a>
-          </li>
-          <li className="icons-list__instagram">
-            <a href="https://www.instagram.com/adamccfc/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={["fab","instagram"]} />
-            </a>
-          </li>
-          <li className="icons-list__github">
-            <a href="https://github.com/adamccfc" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={["fab","github"]} />
-            </a>
-          </li>
-          <li className="icons-list__stackoverflow">
-            <a href="https://stackoverflow.com/users/3276321/adam-morgan" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={["fab","stack-overflow"]} />
-            </a>
-          </li>
-          <li className="icons-list__codepen">
-            <a href="https://codepen.io/AdamCCFC/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={["fab","codepen"]} />
-            </a>
-          </li>
-          <li className="icons-list__linkedin">
-            <a href="https://uk.linkedin.com/in/adam-morgan-230a9672" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={["fab","linkedin"]} />
-            </a>
-          </li>
-        </ul>
+      <div className="home-container__intro">
+        <h1><span className="animation animation__fade-in-down">Adam </span><span className="animation animation__fade-in-down animation__delay--0-5">Morgan</span></h1>
+        <h2><span className="animation animation__fade-in-down animation__delay--1">Front end </span><span className="animation animation__fade-in-down animation__delay--1-5">developer</span></h2>
       </div>
+      <p class="contact animation animation__fade-in-up animation__delay--2">Need a website built? Get in touch.</p>
+      {/* <ul className="icons-list animation animation__fade-in-up animation__delay--2">
+        <li className="icons-list__twitter">
+          <a href="http://www.twitter.com/adamtaffymorgan" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={["fab","twitter"]} />
+          </a>
+        </li>
+        <li className="icons-list__instagram">
+          <a href="https://www.instagram.com/adamccfc/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={["fab","instagram"]} />
+          </a>
+        </li>
+        <li className="icons-list__github">
+          <a href="https://github.com/adamccfc" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={["fab","github"]} />
+          </a>
+        </li>
+        <li className="icons-list__stackoverflow">
+          <a href="https://stackoverflow.com/users/3276321/adam-morgan" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={["fab","stack-overflow"]} />
+          </a>
+        </li>
+        <li className="icons-list__codepen">
+          <a href="https://codepen.io/AdamCCFC/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={["fab","codepen"]} />
+          </a>
+        </li>
+        <li className="icons-list__linkedin">
+          <a href="https://uk.linkedin.com/in/adam-morgan-230a9672" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={["fab","linkedin"]} />
+          </a>
+        </li>
+      </ul> */}
     </div>
   </Layout>
 )
