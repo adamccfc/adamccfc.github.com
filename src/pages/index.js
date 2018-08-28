62a8c2d3cf77b008842f99ffb2928bff034db2e2
amorgan <adam.morgan013@gov.wales>
2018-08-28 16:06:55 +0100

    Change transitions & improve page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,11 @@ const IndexPage = () => (
   <Layout>
     <div className="home">
       <div className="home__intro">
-        <h1><span className="animation animation__fade-in-down">Adam </span><span className="animation animation__fade-in-down animation__delay--0-5">Morgan</span></h1>
-        <h2><span className="animation animation__fade-in-down animation__delay--1">Front end </span><span className="animation animation__fade-in-down animation__delay--1-5">developer</span></h2>
+        <h1><span className="animation animation__fade-in-down">Adam </span><span className="animation animation__fade-in-down animation__delay--0-25">Morgan</span></h1>
+        <h2><span className="animation animation__fade-in-down animation__delay--0-5">Front end </span><span className="animation animation__fade-in-down animation__delay--0-75">developer</span></h2>
       </div>
-      <p className="home__contact animation animation__fade-in-up animation__delay--2-25">Need a website built? Get in touch.</p>
-      <ul className="icons-list animation animation__fade-in-up animation__delay--2-75">
+      <p className="home__contact animation animation__fade-in-up animation__delay--1-5">Need a website built? Get in touch.</p>
+      <ul className="icons-list animation animation__fade-in-up animation__delay--1-5">
           <li className="icons-list__twitter">
             <a href="http://www.twitter.com/adamtaffymorgan" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={["fab","twitter"]} />
